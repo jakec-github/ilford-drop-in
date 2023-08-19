edd1130fd27afbdaced6dcc8317629b96b41020f
Jake Chorley <jakechorley@192.168.1.177>
2023-08-19 15:25:52 +0100

    Makes parseVolunteers resilient to column order changes

--- utils/parseVolunteers.ts
+++ utils/parseVolunteers.ts
@@ -1,13 +1,42 @@
 import { ServiceVolunteer } from '../types';
 
-// TODO: Remove reliability on column indexes
-export const parseVolunteers = (raw: string[][]): ServiceVolunteer[] =>
-  raw.slice(1).map((volunteer) => ({
-    firstName: volunteer[0],
-    lastName: volunteer[1],
-    role: volunteer[2], // TODO: Make enum values enums
-    status: volunteer[3],
-    gender: volunteer[4],
-    email: volunteer[5],
-    groupKey: volunteer[10] || null,
+const FIELDS = [
+  'First name',
+  'Last name',
+  'Role',
+  'Status',
+  'Sex/Gender',
+  'Email',
+  'Group key',
+];
+
+export const parseVolunteers = (raw: string[][]): ServiceVolunteer[] => {
+  const fieldIndexes = new Map<string, number>();
+
+  FIELDS.forEach((field) => {
+    fieldIndexes.set(
+      field,
+      raw[0].findIndex((text) => text === field),
+    );
+  });
+
+  const getField = (field: string, row: string[]): string => {
+    const index = fieldIndexes.get(field);
+
+    if (index === undefined || index === -1) {
+      throw Error(`Missing field in volunteers data: ${field}`);
+    }
+
+    return row[index];
+  };
+
+  return raw.slice(1).map((row) => ({
+    firstName: getField('First name', row),
+    lastName: getField('Last name', row),
+    role: getField('Role', row), // TODO: Make enum values enums or unions
+    status: getField('Status', row),
+    gender: getField('Sex/Gender', row),
+    email: getField('Email', row),
+    groupKey: getField('Group key', row) || null,
   }));
+};
