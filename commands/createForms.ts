5d3f79cbe638eb57dee0a7a195e897c3c1ac40ba
Jake Chorley <jakechorley@192.168.1.209>
2023-09-08 08:37:41 +0100

    Fixes bug in ISO date generation

--- commands/createForms.ts
+++ commands/createForms.ts
@@ -24,8 +24,12 @@ const getNextShifts = (lastShift: string, shiftCount: number) => {
   for (let i = 1; i <= shiftCount; i += 1) {
     const shift = new Date(lastShift);
     shift.setDate(shift.getDate() + i * 7);
+
     dates.push(shift.toDateString());
-    isoDates.push(shift.toISOString());
+
+    // Ensures that BST times still result in an ISO string with the correct date
+    shift.setHours(shift.getHours() + 1);
+    isoDates.push(shift.toISOString().slice(0, 10));
   }
 
   return [dates, isoDates];
--- utils/isoDatesToRange.ts
+++ utils/isoDatesToRange.ts
@@ -1,3 +1,3 @@
 // Assumes dates are in chronological order
 export const isoDatesToRange = (dates: string[]) =>
-  `${dates[0].slice(0, 10)} - ${dates[dates.length - 1].slice(0, 10)}`;
+  `${dates[0]} - ${dates[dates.length - 1]}`;
