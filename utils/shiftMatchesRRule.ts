9dd1719f113c50af2e044d88e5fa73960a8c619a
Jake Chorley <jakechorley@MacBook-Pro-2.local>
2025-08-14 00:27:54 +0200

    Adds rrule matching util

---
+++ utils/shiftMatchesRRule.ts
@@ -0,0 +1,22 @@
+import dayjs, { Dayjs } from 'dayjs';
+import { RRule } from 'rrule';
+
+// TODO: Clear error on invalid rruleString
+export const shiftMatchesRRule = (
+  rruleString: string,
+  shift: Dayjs,
+  start: Dayjs,
+  end: Dayjs,
+) => {
+  const options = RRule.parseString(rruleString);
+  options.dtstart = start.startOf('day').toDate();
+  options.until = end.startOf('day').add(1, 'day').toDate();
+
+  const rrule = new RRule(options);
+
+  return rrule.all().some((date) => {
+    console.log(dayjs(date.toISOString().split('T')[0]).format('YYYY-MM-DD'));
+    console.log(shift.format('YYYY-MM-DD'));
+    return dayjs(date.toISOString().split('T')[0]).isSame(shift);
+  });
+};
